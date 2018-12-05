09b6887fc4eaca51e21f1eb1e5a0c7786a11bed4
Srinivas Rao <srinivas@cloudthing.com>
2018-12-05 16:45:11 +0530

    Logging framework setup

---
+++ Main/Helpers/Logging/FileLogger.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Main
+{
+    public class FileLogger : ILogger
+    {
+        public string FileName { get; }
+
+        public string  Path { get; }
+
+        private FileStream stream;
+
+        public FileLogger(string fileName, string path = null)
+        {
+            if(string.IsNullOrEmpty(path))
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            path = $"{path}/{fileName}";
+            FileName = fileName;
+            Path = path;
+            stream = File.Create(path);
+        }
+
+        public void Log(Exception exception)
+        {
+            var ex = JsonConvert.SerializeObject(exception);
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"");
+            builder.AppendLine($"{DateTime.Now}");
+            builder.AppendLine("-- Exception Happened--");
+            builder.AppendLine($"{ex}");
+            builder.AppendLine($"");
+            //File.WriteAllText(Path, builder.ToString());
+            File.AppendAllText(Path, builder.ToString());
+        }
+
+        public void Log(string message)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"");
+            builder.AppendLine($"{DateTime.Now}");
+            builder.AppendLine($"{message}");
+            File.AppendAllText(Path, builder.ToString());
+        }
+    }
+}
---
+++ Main/Helpers/Logging/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    public interface ILogger
+    {
+        void Log(Exception exception);
+        void Log(string message);
+    }
+}
