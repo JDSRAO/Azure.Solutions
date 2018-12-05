4be898d0e73dab093ee92f82afd4a2fa86da9b36
Srinivas Rao <srinivas@cloudthing.com>
2018-12-05 14:34:05 +0530

    Logging helpers

---
+++ Main/Helpers/Logger.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    public static class  Logger
+    {
+        public static void Log(Exception exception)
+        {
+            string ex = JsonConvert.SerializeObject(exception);
+            Console.WriteLine($"{ex}");
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+    }
+}
