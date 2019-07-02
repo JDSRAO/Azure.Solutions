ea94225776690081ffa19ddd793d1408f05fd5e6
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-07-02 15:01:14 +0530

    Service bus XML comments

--- ServiceBus/Implementations/ServiceBusTopic.cs
+++ ServiceBus/Implementations/ServiceBusTopic.cs
@@ -12,10 +12,19 @@ namespace ServiceBus.Implementations
         private static ITopicClient topicClient;
         private static ISubscriptionClient subscriptionClient;
 
+        /// <summary>
+        /// Service bus topic connection string
+        /// </summary>
         public string ServiceBusConnectionString { get; }
 
+        /// <summary>
+        /// Topic name
+        /// </summary>
         public string EntityName { get; }
 
+        /// <summary>
+        /// Message received event handler
+        /// </summary>
         public event EventHandler<MessageReceivedArgs> MessageReceived;
 
         public ServiceBusTopic(string connectionString, string topicName, string subscriptionName)
@@ -31,6 +40,12 @@ namespace ServiceBus.Implementations
             RegisterMessageHandler();
         }
 
+        /// <summary>
+        /// Sends message to topic
+        /// </summary>
+        /// <param name="message">Message to send</param>
+        /// <param name="useSessions"></param>
+        /// <returns></returns>
         public async Task SendMessageAsync(string message, bool useSessions = false)
         {
             try
@@ -46,6 +61,9 @@ namespace ServiceBus.Implementations
 
         #region Private Methods
 
+        /// <summary>
+        /// Registers the topic client for subscription
+        /// </summary>
         private void RegisterMessageHandler()
         {
             var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
@@ -56,6 +74,12 @@ namespace ServiceBus.Implementations
             subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
         }
 
+        /// <summary>
+        /// Receives message and invokes the message received handler
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         private async Task ProcessMessagesAsync(Message message, CancellationToken token)
         {
             try
@@ -69,6 +93,11 @@ namespace ServiceBus.Implementations
             }
         }
 
+        /// <summary>
+        /// Excetion handler 
+        /// </summary>
+        /// <param name="exceptionReceivedEventArgs"></param>
+        /// <returns></returns>
         private async static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
         {
             await subscriptionClient.AbandonAsync($"{ReceiveMode.PeekLock}");
