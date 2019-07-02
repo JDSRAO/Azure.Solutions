9ce9fe2d6e822ce6363f887dda7c55240328e1f8
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-07-02 17:57:36 +0530

    Table storage base entity refactor

--- Main/StorageAccount/TableProgram/Entities/CustomerEntity.cs
+++ Main/StorageAccount/TableProgram/Entities/CustomerEntity.cs
@@ -7,16 +7,15 @@ namespace Main.StorageAccount.TableProgram.Entities
 {
     public class CustomerEntity : BaseEntity
     {
-        public override string TableName => "customerData";
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
-        public CustomerEntity()
+        public CustomerEntity() : base("customerData")
         {
 
         }
 
-        public CustomerEntity(string department)
+        public CustomerEntity(string department) : base("customerData")
         {
             PartitionKey = department;
             RowKey = Guid.NewGuid().ToString();
